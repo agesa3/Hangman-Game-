c79b11fd7f314d92cef527d7c181f6adcb53627a
agesa3 <collinsagesa2000@gmail.com>
2022-01-24 19:18:09 +0300

    wrote hangman logic

--- src/com/agesadev/Hangman.java
+++ src/com/agesadev/Hangman.java
@@ -11,16 +11,39 @@ public class Hangman {
 
     public static void main(String[] args) throws FileNotFoundException {
         //read the txt file with english words
-        Scanner scanner = new Scanner(new File(""));
+        Scanner scanner = new Scanner(new File("C:\\Users\\Agesa\\Documents\\workspace\\Hangman Game\\words.txt"));
+        Scanner userInput = new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
             //add the text to a string array list
             words.add(scanner.nextLine());
+
         }
         Random rand = new Random();
         String word = words.get(rand.nextInt(words.size()));
 
+        System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+        printWordState(word, playerGuesses);
+        System.out.println("Please Enter a letter: ");
+        String userGuess = userInput.nextLine();
+        playerGuesses.add(userGuess.charAt(0));
+        printWordState(word, playerGuesses);
 
     }
+
+    private static void printWordState(String word, List<Character> playerGuesses) {
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+    }
+
+
 }
